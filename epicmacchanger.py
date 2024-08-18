f9c317435e5016f419460ce45d3b03ca1ca60fc1
Mete Han Without Han <56835980+metolius25@users.noreply.github.com>
2024-08-19 01:09:52 +0300

    Update epicmacchanger.py

--- epicmacchanger.py
+++ epicmacchanger.py
@@ -6,8 +6,8 @@ parser = agp.ArgumentParser(
                     prog='epicmacchanger.py',
                     description='Simple Python 3 app to specify a new MAC address.\nPro tip: use ip addr to find which interface you want to work on.\nRequires superuser.',
                     epilog='Example usage: python3 mac_changer.py -i eth0 -m 00:H1:E2:L3:L4:O5 (Hexadecimal characters only)')
-parser.add_argument("-i", "--interface", dest="interface", help="Choose the network interface you want to work on", required=True) 
-parser.add_argument("-m", "--mac", dest="mac_address", help="Write new MAC Address", required=True)
+parser.add_argument("-i", "--interface", dest="interface", help="Choose the network interface you want to work on\n", required=True) 
+parser.add_argument("-m", "--mac", dest="mac_address", help="Write new MAC Address\n", required=True)
 
 args = parser.parse_args()
 interface_var = args.interface
