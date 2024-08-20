a35323dbf094dc1039656b531c12ff13bc197c6d
Mete Han Without Han <56835980+metolius25@users.noreply.github.com>
2024-08-21 00:30:36 +0300

    Update epicmacchanger.py

--- epicmacchanger.py
+++ epicmacchanger.py
@@ -7,8 +7,8 @@ import re
 def get_Input():
     parser = agp.ArgumentParser(
                     prog='epicmacchanger.py',
-                    description='Simple Python app to specify a new MAC address.\nPro tip: use ip addr to find which interface you want to work on.\nRequires superuser.',
-                    epilog='Example usage: python3 mac_changer.py -i eth0 -m 00:H1:E2:L3:L4:O5 (Hexadecimal characters only)')
+                    description='Simple Python app to specify a new MAC address. Likely not going to work on virtual machines. Restart your PC if kills your internet connection.',
+                    epilog='Example usage: python3 epicmacchanger.py -i eth0 -m 00:H1:E2:L3:L4:O5 (Hexadecimal characters only)')
     parser.add_argument("-i", "--interface", dest="interface", help="Choose the network interface you want to work on\n", required=True) 
     parser.add_argument("-m", "--mac", dest="mac_address", help="Write new MAC Address\n", required=True)
     (args) = parser.parse_args()
@@ -18,12 +18,11 @@ def change_Address(interface_var, mac_var):
     subprocess.call(["sudo", "ip", "link", "set", "dev",interface_var, "down"])
     subprocess.call(["sudo", "ip", "link", "set", "dev", interface_var, "address", mac_var])
     subprocess.call(["sudo", "ip", "link", "set", "dev", interface_var, "up"])
-    subprocess.call(["sudo", "dhclient", interface_var])
 
 
 def control_Mac(iface):
 
-    ip_addr = subprocess.check_output(["ip", "addr", "show", iface])
+    ip_addr = subprocess.check_output(["sudo", "ifconfig", iface])
     new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ip_addr))
     if new_mac:
         output = new_mac.group(0)
@@ -44,4 +43,3 @@ if (final_mac == args.mac_address):
 else:
     print(f"It appears the mac address you typed ({args.mac_address}) is invalid. Try again")
     
-
